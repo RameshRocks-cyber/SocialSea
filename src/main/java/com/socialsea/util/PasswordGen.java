bceec530fe0a523575260fb41e7efdff79cfd2ce
Ramesh <rameshnanda485@gmail.com>
2026-01-09 14:52:00 +0530

    Enable Hibernate ddl-auto update and add password generator

---
+++ src/main/java/com/socialsea/util/PasswordGen.java
@@ -0,0 +1,9 @@
+package com.socialsea.util;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordGen {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("admin123"));
+    }
+}
