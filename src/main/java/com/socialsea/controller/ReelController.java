078a4602af7f205d0f254a53b1037ba275bfda13
Ramesh <rameshnanda485@gmail.com>
2026-01-09 10:04:49 +0530

    Fix ReelController imports

--- src/main/java/com/socialsea/controller/ReelController.java
+++ src/main/java/com/socialsea/controller/ReelController.java
@@ -1,5 +1,15 @@
 package com.socialsea.controller;
 
+import com.socialsea.model.Post;
+import com.socialsea.repository.PostRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/reels")
 @CrossOrigin
