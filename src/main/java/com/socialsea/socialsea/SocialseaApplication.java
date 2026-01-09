af2af13b6cb4ecbfc25c85eb1ad393dd8b1a8513
Ramesh <rameshnanda485@gmail.com>
2026-01-09 10:44:17 +0530

    Fix entity scanning for JPA

--- src/main/java/com/socialsea/socialsea/SocialseaApplication.java
+++ src/main/java/com/socialsea/socialsea/SocialseaApplication.java
@@ -1,13 +1,14 @@
-package com.socialsea.socialsea;
+package com.socialsea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.socialsea.model")
 public class SocialseaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocialseaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SocialseaApplication.class, args);
+    }
 }
