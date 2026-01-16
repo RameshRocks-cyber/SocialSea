eb6def4e4151047c258c26e9e612c0ea03ecc479
Ramesh <rameshnanda485@gmail.com>
2026-01-16 09:17:14 +0530

    Fix Railway DB auto configuration

--- src/main/java/com/socialsea/SocialseaApplication.java
+++ src/main/java/com/socialsea/SocialseaApplication.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.socialsea.repository")
-@EntityScan(basePackages = "com.socialsea.model")
 public class SocialseaApplication {
 
     public static void main(String[] args) {
