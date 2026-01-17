365b371d15303d7b8fc82c9f96c6a0fff5e4e968
Ramesh <rameshnanda485@gmail.com>
2026-01-17 15:27:07 +0530

    Fix User.getPassword() build error

--- src/main/java/com/socialsea/model/User.java
+++ src/main/java/com/socialsea/model/User.java
@@ -1,16 +1,9 @@
 package com.socialsea.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "app_user")
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
 
     @Id
@@ -25,4 +18,44 @@ public class User {
     private String role = "USER"; // USER | ADMIN
 
     private boolean banned = false;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isBanned() {
+        return banned;
+    }
+
+    public void setBanned(boolean banned) {
+        this.banned = banned;
+    }
 }
