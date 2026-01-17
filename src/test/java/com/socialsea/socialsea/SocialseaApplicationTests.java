0595a64f0d41f2eb7c651f82ea719bc77dd8f070
Ramesh <rameshnanda485@gmail.com>
2026-01-17 16:55:30 +0530

    Fix mail config for tests

--- src/test/java/com/socialsea/socialsea/SocialseaApplicationTests.java
+++ src/test/java/com/socialsea/socialsea/SocialseaApplicationTests.java
@@ -1,13 +1,19 @@
-package com.socialsea.socialsea;
+package com.socialsea;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+        "spring.mail.host=localhost",
+        "spring.mail.port=25",
+        "spring.mail.username=test",
+        "spring.mail.password=test"
+    }
+)
 class SocialseaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
