c73ed2f6920c2e0ce03886b835ebfc3a8b257ce8
Ramesh <rameshnanda485@gmail.com>
2026-01-31 14:01:35 +0530

    change of application-prod.properties

--- src/main/java/com/socialsea/service/EmailService.java
+++ src/main/java/com/socialsea/service/EmailService.java
@@ -1,43 +1,71 @@
 package com.socialsea.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import okhttp3.*;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    @Value("${RESEND_API_KEY}")
+    private String apiKey;
 
     @Value("${MAIL_FROM}")
     private String from;
 
-    private static final Logger log = LoggerFactory.getLogger(EmailService.class);
+    private static final String RESEND_URL = "https://api.resend.com/emails";
+
+    @Async
+    public void sendOtpEmail(String to, String otp) {
+
+        String json = """
+        {
+          "from": "%s",
+          "to": ["%s"],
+          "subject": "Your SocialSea OTP",
+          "html": "<h2>Your OTP</h2><p><b>%s</b></p>"
+        }
+        """.formatted(from, to, otp);
+
+        sendRequest(json);
+    }
+
+    public void send(String to, String subject, String body) {
+        String json = """
+        {
+          "from": "%s",
+          "to": ["%s"],
+          "subject": "%s",
+          "html": "<p>%s</p>"
+        }
+        """.formatted(from, to, subject, body);
+
+        sendRequest(json);
+    }
 
     public void sendEmail(String to, String subject, String body) {
         send(to, subject, body);
     }
 
-    public void send(String to, String subject, String body) {
-        log.info("Sending email to {}", to);
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(from);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
-    }
+    private void sendRequest(String json) {
+        Request request = new Request.Builder()
+                .url(RESEND_URL)
+                .post(RequestBody.create(json, MediaType.parse("application/json")))
+                .addHeader("Authorization", "Bearer " + apiKey)
+                .addHeader("Content-Type", "application/json")
+                .build();
 
-    @Async
-    public void sendOtpEmail(String to, String otp) {
-        String subject = "Your SocialSea OTP";
-        String body = "Your OTP is: " + otp + "\n\nValid for 5 minutes.";
-        send(to, subject, body);
+        OkHttpClient client = new OkHttpClient();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new RuntimeException("Email failed: " + response.body().string());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Email send error", e);
+        }
     }
 }
