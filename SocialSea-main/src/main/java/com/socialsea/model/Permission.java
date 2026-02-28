e8c81f565d0db2cacbf01672cb678e4535f19882
Ramesh <rameshnanda485@gmail.com>
2026-02-28 10:40:26 +0530

    Remove duplicate nested backend

--- SocialSea-main/src/main/java/com/socialsea/model/Permission.java
+++
@@ -1,9 +0,0 @@
-package com.socialsea.model;
-
-public enum Permission {
-    USER_READ,
-    USER_BAN,
-    POST_REVIEW,
-    REPORT_RESOLVE,
-    DASHBOARD_VIEW
-}
--- SocialSea-main/src/test/java/com/socialsea/socialsea/SocialseaApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.socialsea.socialsea;
-
-import org.junit.jupiter.api.Disabled;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@Disabled("Disabled until test DB is configured")
-@SpringBootTest
-class SocialseaApplicationTests {
-}
